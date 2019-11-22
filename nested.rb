e7373e3032cd5010cf398b58967e6fc3bc05240f
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-22 05:16:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,10 +16,12 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
+ 
+ 
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  #new_a = {}
+  new_a = programmer_hash[:dennis_ritchie][:languages][0]
+  
+  
 end
 
 def adding_matz
@@ -79,7 +86,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  hash1 = {}
+hash1[:yukihiro_matsumoto]={:known_for =>"Ruby",:languages => ["LISP","C"]}
+hash1.merge(programmer_hash)
     
 end
 
@@ -102,7 +111,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
